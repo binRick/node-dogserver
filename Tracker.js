86c04c903c3302ee9f7f85db7f297c2912ebf1bc
binRick <rapidvps@gmail.com>
2015-06-14 18:49:25 -0400

    tracker idea

---
+++ Tracker.js
@@ -0,0 +1,20 @@
+var gpstracker = require("gpstracker");
+var server = gpstracker.create().listen(8000, function(){
+    console.log('listening your gps trackers on port', 8000);
+});
+
+server.trackers.on("connected", function(tracker){
+
+    console.log("tracker connected with imei:", tracker.imei);
+
+    tracker.on("help me", function(){
+        console.log(tracker.imei + " pressed the help button!!".red);
+    });
+
+    tracker.on("position", function(position){
+        console.log("tracker {" + tracker.imei +  "}: lat", 
+                            position.lat, "lng", position.lng);
+    });
+
+    tracker.trackEvery(10).seconds();
+});
